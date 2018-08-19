68bf7a27563639fc6c898adfdaf36f07e0d6f930
aaronpalermo <github@aaronpalermo.com>
2018-08-19 15:43:02 -0400

    moved from display filter to capture filter and added status bar

--- capture.py
+++ capture.py
@@ -1,29 +1,46 @@
+#!/usr/bin/python3
+
 import pyshark
 import hashlib
 import time
-capture = pyshark.LiveCapture(interface='wlan1mon', display_filter='wlan.ta_resolved contains "c3:58"')
+import requests
+
+capture = pyshark.LiveCapture(interface='wlan1mon', bpf_filter='wlan src a8:96:75:c3:58:0d')
+status = '.'
+print("Sending packets to web server")
 
 for packet in capture.sniff_continuously():
-  #pretty_print('Just arrived:', packet)
-  #packet.pretty_print()
   try:
     pkt_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(packet.frame_info.time_epoch))) + str(packet.frame_info.time_epoch)[10:]
   except:
-    pkt_time = "no data in pkt"
-  
+    pkt_time = "no time in pkt"
+
   try:
     pkt_ta = packet.wlan.ta_resolved
   except:
-    pkt_ta = "no data in pkt"
-    
-  try: 
+    pkt_ta = "no mac in pkt"
+
+  try:
     pkt_rssi = packet.radiotap.dbm_antsignal
   except:
-    pkt_rssi = "no data in pkt"
-    
+    pkt_rssi = "no dbm in pkt"
+
   try:
     pkt_data_md5 = hashlib.md5(str(packet.data.data).encode('utf-8')).hexdigest()
   except:
     pkt_data_md5 = "no data in pkt"
-    
-  print('{0},{1},{2},{3}'.format(pkt_time, pkt_ta, pkt_rssi, pkt_data_md5))
+
+  #print('{0},{1},{2},{3}'.format(pkt_time, pkt_ta, pkt_rssi, pkt_data_md5))
+  payload = '{0},{1},{2},{3}'.format(pkt_time, pkt_ta, pkt_rssi, pkt_data_md5)
+  #print(payload)
+  print(status, end='\r')
+  if len(status) < 80:
+    status = status + '.'
+  else:
+    print("                                                                                     ", end='\r')
+    status = '.'
+
+  try:
+    r = requests.put("http://172.24.1.1:8000", data=payload)
+  except:
+    print("Error contacting web server")
